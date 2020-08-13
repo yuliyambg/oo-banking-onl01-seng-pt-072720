ca40aadcbfe00ce4972cc5233de3f0621f692e84
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-13 00:41:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,28 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+def valid?
+  @sender.valid? && @receiver.valid?
+end
+
+def execute_transaction
+  if sender.balance < amount || sender.status == "closed" || receiver.status == "closed"
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+  end 
+  if self.status != "complete"
+    sender.deposit(-amount)
+    receiver.deposit(amount)
+    self.status = "complete"
+  end
+end
+
 end
